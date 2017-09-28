0c081077d0c6c4f0dc9ec0745329ac6bb9391be5
Dmytro Ababii <ababiids@gmail.com>
2017-09-28 15:19:12 +0300

    Updated skip_before_action for posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:index]
+  
   def index
     posts_id = []
     
